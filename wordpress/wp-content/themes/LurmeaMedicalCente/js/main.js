4224a1364a862bdb566c5a4fda18e6da4b48069a
leelamhair <leelamhair@gmail.com>
2016-03-24 13:50:57 +0700

    Create file 404 and 500, add database new

--- wordpress/wp-content/themes/LurmeaMedicalCente/js/main.js
+++
@@ -1,7 +0,0 @@
-var height = $("#content").height();
-var container_width= $(".container").width();
-console.log(container_width);
-var resultHeight = height + 50;
-if (container_width >= 940) {
-	$("#main-left").css("height", resultHeight);
-}
